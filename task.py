662984e258adea6847eb54b1afab9a902e1bc715
KapilKataria-nix <kapilkataria245@gmail.com>
2024-02-03 11:18:15 +0530

    Task initialized

---
+++ task.py
@@ -0,0 +1,16 @@
+import csv, random
+from datetime import datetime, timedelta
+
+def random_data():
+    order_id = random.randint(1000, 9999)
+    customer_id = random.randint(100, 999)
+    order_date = (datetime.now() - timedelta(days=random.randint(1, 365))).strftime('%d-%m-%y')
+    product_id = random.randint(1, 100)
+    product_name = f"Product{product_id}"
+    product_price = round(random.uniform(10, 1000), 2)
+    quantity = random.randint(1, 10)
+
+    return [order_id, customer_id, order_date, product_id, product_name, product_price, quantity]
+
+
+
